bd1a3bc01fdf4da5894d75dd8f465d1bec1e69f1
ESC18 <ponderosaprintingus@gmail.com>
2023-12-22 15:00:56 -0800

    Add additional styling:

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@
 import './assets/images/SC.png';
 import './assets/fonts.css';
 import './css/styles.css';
+import './css/rings.css';
 import './js/secondary.js';
 //EX: import Blank from '/.js/secondary.js';
