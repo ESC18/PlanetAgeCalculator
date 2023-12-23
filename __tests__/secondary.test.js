2df74f9bb44855a9e0bfa9c5aa31ece6ab5e3b7f
ESC18 <ponderosaprintingus@gmail.com>
2023-12-22 16:47:58 -0800

    clean up comments

--- __tests__/secondary.test.js
+++ __tests__/secondary.test.js
@@ -1,21 +1,3 @@
-// Example of test below complete with import statement.
-/*
-import Blank from './../src/rectangle.js';
-
-describe("Blank", () => {
-    let blank; 
-    
-    beforeEach(() => {
-        blank = new Blank(x,y);
-    })
-
-    test("blah blah blah blah blah", () => {
-        expect(Blank.x).toEqual(xx);
-        expect(Blank.y).toEqual(yy);
-    });
-})
-*/
-
 import { planetAgeCalc } from "../src/js/secondary.js";
 
 describe("planetAgeCalc", () => {
